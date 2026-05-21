9a992c310eb62d364cb21fa4758826201fa5172c
Akhila110 <kandulaakhila1@gmail.com>
2026-05-21 16:21:50 +0530

    Add files via upload

---
+++ weather_app.py
@@ -0,0 +1,15 @@
+import requests
+
+city = input("Enter city name: ")
+
+url = f"https://wttr.in/{city}?format=j1"
+
+response = requests.get(url)
+data = response.json()
+
+current = data["current_condition"][0]
+
+print("\nWeather Report")
+print("Temperature:", current["temp_C"], "°C")
+print("Humidity:", current["humidity"], "%")
+print("Weather:", current["weatherDesc"][0]["value"])
